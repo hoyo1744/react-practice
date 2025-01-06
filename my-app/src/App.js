1a920e71d60dba93aa8c84d810a4efed9d562920
hoyo1744 <hoyo1744@naver.com>
2025-01-06 21:10:57 +0900

    #6.0 Introduction

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,13 +2,26 @@ import Button from "./Button";
 import styled from "./App.module.css"
 
 
+// App의 state 변경될때마다 리로드된다. 만약 최초 1번만 리로드하고싶다면?
+// 그런 경우는 많지! api 호출은 딱 1번만 이루어져야하니까
+// 그럼 어떻게 해야 처음에만 로딩될까?
+// 일반적으로 sate변화되면 전부 리로드 된다. 왜냐하면 업데이트된 Value를 봐야하니까 알아서 리로드하는거임. 어떨때? State가 변경될때
+
 function App() {
-  return (
-    <div>
-      <h1 className={styled.title}>Welcome back!</h1>
-      <Button text={"Continue"} />
-    </div>
-  );
+
+    const [counter, setValue] = React.useState(0);
+
+    const onClick = () => {
+        setValue((prev) => prev + 1);
+    }
+    console.log("render)");
+    return (
+        <div>
+            <h1>{counter}</h1>
+            <button onClick={onClick}>click me</button>
+
+        </div>
+    );
 }
 
 export default App;
