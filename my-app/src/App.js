552710922fef648b0cd167e771e8fce2e3897632
hoyo1744 <hoyo1744@naver.com>
2025-01-09 21:47:45 +0900

    #7.7 Publishing

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -9,9 +9,9 @@ import Home from "./routes/Home";
 import Detail from "./routes/Detail";
 
 
-// 7-6 동적 url
-// "/movie/:id :id는 변수임. :를 붙여야변수가 된다.
-// url에서 id를 정보를 detail 컴포넌특 어떻게알아갈수있을까'?
+// 7-7
+// gh-pages는 github에서 무료 도메인을 배포해준다.
+
 
 function App() {
     return <Router>
