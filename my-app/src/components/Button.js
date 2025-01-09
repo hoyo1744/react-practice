3ff2538b443a09c5c653c74a1b165d00ab9ec382
hoyo1744 <hoyo1744@naver.com>
2025-01-09 20:48:40 +0900

    #7.4 Movie App part Two

--- my-app/src/components/Button.js
+++ my-app/src/components/Button.js
@@ -1,7 +1,7 @@
 import PropTypes from "prop-types";
 // CSS 코드를 import해서 사용함.
 // CSS 코드도 모듈러가 될 수 있다.(.module.css를 반드시 붙여야함.)
-import styled from"./Button.module.css";
+import styled from "../Button.module.css";
 
 function Button({text}){
     return <button className={styled.btn}>
