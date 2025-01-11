67f48a0626348db5a326cf3aa52cbe5f7e27a385
hoyo1744 <hoyo1744@naver.com>
2025-01-11 16:23:20 +0900

    #7.7 코드 챌린지

--- my-app/src/routes/Detail.js
+++ my-app/src/routes/Detail.js
@@ -1,22 +1,19 @@
 import {useEffect, useState} from "react";
 import {useParams} from "react-router-dom";
+import Movie from "../components/Movie";
 
 // useParams을 사용해서 url 변수를 알아올수있다.
 // await은 async 안에있어야 사용할 수 있다.
 
 function Detail(){
     const [loading, setLoading] = useState(true);
-    const [movie, setMovie] = useState()
-
-
+    const [movie, setMovie] = useState(null)
     const {id} = useParams();
-    console.log(id);
 
     const getMovie = async() => {
         const json = await(
             await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
         ).json();
-        console.log(json);
         setLoading(false);
         setMovie(json);
     }
@@ -24,9 +21,21 @@ function Detail(){
         getMovie();
 
         setLoading(true);
+
     }, []);
     return <div>
-    {/*    todo*/}
+    {
+
+        loading ? <h1>Loading....</h1> :
+            <div>
+                <Movie
+                    id={movie.data.movie.id}
+                    coverImg={movie.data.movie.medium_cover_image}
+                    title={movie.data.movie.title}
+                    summary={movie.data.movie.summary}
+                    genres={movie.data.movie.genres}/>
+            </div>
+    }
 
     </div>
 }
